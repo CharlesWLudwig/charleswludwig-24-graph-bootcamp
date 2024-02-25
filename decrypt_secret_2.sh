577602c67cab6f8e89fcf7b236308290d2a6b96e
CharlesWLudwig <charleswludwig@gmail.com>
2024-02-25 16:18:23 -0500

    adding new decryption script

--- decrypt_secret_2.sh
+++ decrypt_secret_2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $HOME/secretstwo
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_2" \
---output $HOME/secrets/Neo4j.cs Neoflix/Neo4j.cs.gpg
+--output $HOME/secretstwo/Neo4j.cs Neoflix/Neo4j.cs.gpg
