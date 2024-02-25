0c48612a2800b9dd6767f9a9c2f17aa6df671389
Charles Ludwig <charleswludwig@gmail.com>
2024-02-25 13:54:49 -0500

    updating Program.cs

--- Neoflix/Program.cs
+++ Neoflix/Program.cs
@@ -1,7 +1,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Neo4j.Driver;
+  
 namespace Neoflix
 {
     public class Program
@@ -17,6 +21,23 @@ namespace Neoflix
             // configure and run website
             await CreateHostBuilder(args).Build().RunAsync();
         }
+
+        var cypherQuery =
+        @"
+        MATCH (m:Movie {title:$movie})<-[:RATED]-(u:User)-[:RATED]->(rec:Movie)
+        RETURN distinct rec.title AS recommendation LIMIT 20
+        ";
+
+        var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
+        var result = await session.ReadTransactionAsync(async tx => {
+        var r = await tx.RunAsync(cypherQuery, 
+                new { movie="Crimson Tide"});
+        return await r.ToListAsync();
+        });
+
+        await session?.CloseAsync();
+        foreach (var row in result)
+        Console.WriteLine(row["recommendation"].As<string>());
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
